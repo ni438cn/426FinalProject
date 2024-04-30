f2d1d06123997cc2dd67f7f3e9cb0357c275f06d
cnnrmgdwn <111512653+cnnrmgdwn@users.noreply.github.com>
2024-04-30 16:12:25 -0400

    Added Registration Page and functionality between two pages

--- 426-final-project/src/App.js
+++ 426-final-project/src/App.js
@@ -1,18 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import RegisterPage from './RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>COMP426 Final Project</h1>
-      <div class="input-container">
-        <input class="input" type="text" id="username" placeholder="Username"></input>
-        <input class="input" type="password" id="password" placeholder="Password"></input>
+    <Router>
+      <div className="App">
+        <h1>COMP426 Final Project</h1>
+        <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={
+            <div>
+              <div className="input-container">
+                <input className="input" type="text" id="username" placeholder="Username"></input>
+                <input className="input" type="password" id="password" placeholder="Password"></input>
+              </div>
+              <div className="button-container">
+                <button className="button">Sign In</button>
+              </div>
+              <div className="register-container">
+                <p>Don't have an account? <a href="/register">Register</a></p>
+              </div>
+            </div>
+          } />
+        </Routes>
       </div>
-      <div class="button-container">
-        <button class="button">Register</button>
-        <button class="button">Sign In</button>
-      </div>
-    </div>
+    </Router>
   );
 }
 
